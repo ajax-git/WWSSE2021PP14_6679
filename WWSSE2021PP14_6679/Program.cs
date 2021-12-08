d11a88b89d8938720c82da5ff7940ed4cd0a0ea2
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-08 13:11:28 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP14_6679/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+    Napisz program czytający ze standardowego wejścia kolejne liczby całkowite i oblicza wartość sumy 
+    i średniej arytmetycznej na standardowym wyjściu.
+*/
+
+int number, sum = 0;
+Console.Write("Wprowadź n: ");
+number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    sum += i;
+}
+Console.WriteLine("Suma liczb wynosi: {0}.", sum);
+Console.WriteLine("Średnia arytmetyczna wynosi: {0}.", sum/number);
